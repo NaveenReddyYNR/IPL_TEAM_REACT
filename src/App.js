5387f81750a40294277824971197a14f30f04dd2
NaveenReddyYNR <100398391+NaveenReddyYNR@users.noreply.github.com>
2022-06-12 17:43:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import IplTeams from "./IPLTEAMS/IplTeams";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <IplTeams />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
